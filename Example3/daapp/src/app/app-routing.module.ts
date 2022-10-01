5f77b981e4d00565749800478038f74a8b052677
gsnmax2 <gsnmax2@gmail.com>
2022-10-01 15:41:19 +0530

    ex3

---
+++ Example3/daapp/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+
+// const routes: Routes = []; // sets up routes constant where you define your routes
+
+const routes: Routes = [
+    { path: 'home', component: HomeComponent},
+    { path: 'about-us', component: AboutUsComponent },
+    { path: 'contact-us', component: ContactUsComponent}
+  ];
+
+
+// configures NgModule imports and exports
+@NgModule({
+  declarations: [ 
+    AboutUsComponent, ContactUsComponent, HomeComponent
+  ],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
